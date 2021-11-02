f8d04120176f40fdb2f5408b5f84650db667cc84
iamsanketdhumal <92759788+iamsanketdhumal@users.noreply.github.com>
2021-11-02 09:04:51 +0000

    instatiated web3

--- contract.js
+++ contract.js
@@ -1,9 +1,13 @@
 //this is a basic readonly contract.js file
 
-// https://ropsten.infura.io/v3/aa7618f10cf94c0cb31ab2cff769b059
+
 const Web3 = require("web3")
 
 //instantiate web3
+
+const rpcURL="https://ropsten.infura.io/v3/";
+const web3= new Web3(rpcURL);
+
 //get the ABI (interface) for our contract
 //connect to our contract on ropsten
 //run some of the methods in our contract using javascript
